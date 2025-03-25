8f0f41b7a5eaf3f594d9ac769bdfdc6321b9e0f5
ThanigaK <122258658+ThanigaK@users.noreply.github.com>
2025-03-25 20:02:00 +0530

    initial setup

---
+++ src/test/java/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+    //Methods
+    //Memory
+    //Keywords [new , static]
+    //Parameter, arguments
+    //stack & heap
+    //Data type
+
+    static String URL = "www.google.com";
+
+    public static void main(String[] args) {
+        System.out.println("Project setup done");
+
+    }
+
+}
